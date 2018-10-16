765ae291e2c40a843533692b827eedbdd43b76b9
dmesparza <dmesparza@gmail.com>
2018-10-15 21:40:43 -0400

    Initial Commit

---
+++ elections/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  elections
+//
+//  Created by Dominic Ruiz-Esparza on 10/15/18.
+//  Copyright © 2018 Dominic Ruiz-Esparza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
